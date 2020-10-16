882799aeb5062932aa70c1b36f13ecdb785aef2f
Grzegorz Łucki <glucki@teonite.com>
2020-10-16 02:32:22 +0200

    Enter credentials manually

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from src.bot import Bot
-from src.config import USER, PLAYER
+from src.config import USER, PLAYER, LOGIN_MANUALLY
 
 bot = Bot()
-bot.login(USER)
+if LOGIN_MANUALLY:
+    bot.login_manually()
+else:
+    bot.login(USER)
+
 bot.buy_player(PLAYER["name"], PLAYER["cost"])
 
 
